ac853cfe069babef0efad1cbc16852106c32301e
Mahmoud Mahmoud <54012843+Mahmoud217TR@users.noreply.github.com>
2022-09-12 00:56:36 +0300

    before seperation

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use App\Models\Traits\MediableTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Game extends Model
 {
-    use HasFactory;
+    use HasFactory, MediableTrait;
+
+    protected $with = ['media'];
 }
